1cbdb61bf07138c4a7657259131c7da89044ec4f
shalevatias <shalev1704@gmail.com>
2026-02-14 15:35:46 +0200

    create project structure

---
+++ app/src/main/java/com/studygram/utils/Constants.kt
@@ -0,0 +1,20 @@
+package com.studygram.utils
+
+object Constants {
+    const val USERS_COLLECTION = "users"
+    const val POSTS_COLLECTION = "posts"
+    const val COMMENTS_COLLECTION = "comments"
+
+    const val PREF_NAME = "studygram_prefs"
+    const val PREF_LAST_SYNC = "last_sync_timestamp"
+    const val PREF_USER_ID = "user_id"
+
+    const val STORAGE_PROFILE_IMAGES = "profile_images"
+    const val STORAGE_POST_IMAGES = "post_images"
+
+    const val MAX_IMAGE_SIZE = 5 * 1024 * 1024
+
+    const val FIELD_LAST_UPDATED = "lastUpdated"
+    const val FIELD_TIMESTAMP = "timestamp"
+    const val FIELD_USER_ID = "userId"
+}
